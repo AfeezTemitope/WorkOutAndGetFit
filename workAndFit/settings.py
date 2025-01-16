863e77dfd870498fdc1aa30e9db9506241915fe6
hafeezco75@gmail.com <hafeezco75@gmail.com>
2025-01-16 15:42:03 +0100

    Implemented exercise types and instructions with calories burned functionalities

--- workAndFit/settings.py
+++ workAndFit/settings.py
@@ -87,11 +87,12 @@ AUTH_USER_MODEL = 'workoutApp.CustomUser'
 STRAVA_AUTH_URL = "https://www.strava.com/oauth/authorize"
 STRAVA_REDIRECT_URI = 'http://localhost:8000/strava/callback'
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'belloafeez28@gmail.com'
+EMAIL_HOST_USER = 'hafeezco75@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 
 
