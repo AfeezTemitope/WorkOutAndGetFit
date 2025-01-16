00a7e3f5ed037f806e2f971c6413c0866b9133b9
hafeezco75@gmail.com <hafeezco75@gmail.com>
2025-01-16 14:22:14 +0100

    Created tests for user register, login,fitness data receiver and sending

--- workoutApp/tests.py
+++ workoutApp/tests.py
@@ -5,9 +5,26 @@ from django.test import TestCase
 from django.test import Client
 
 from workoutApp import models
+from workoutApp.views import CustomUserViewSet
 
 # Create your tests here.
 
 crsf_client = Client(enforce_csrf_checks=True)
 def test_that_fitness_data_received(client):
-    assert models.CustomUser.objects.exists == true
+    assert models.CustomUser.objects.exists == True
+
+def test_that_fitness_data_sent(client):
+    assert models.CustomUser.objects.count() == 1
+
+def test_that_user_can_register(customUser):
+    customUser.set_password('<PASSWORD>')
+    customUser.set_email('walex5347@gmail.com')
+    customUser.set_first_name('John')
+    customUser.set_last_name('Doe')
+    customUser.set_height(6.23)
+    customUser.set_weight(10)
+    customUser.set_age(20)
+    customUser.set_gender('female')
+    response = customUser.save()
+    assert models.CustomUser.objects.count() == 1
+    assert response == "User has been successfully registered"
