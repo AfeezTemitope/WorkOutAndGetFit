5b1b8587fb25e6856ace7e74fee73298e7631068
hafeezco75@gmail.com <hafeezco75@gmail.com>
2025-01-18 23:21:07 +0100

    Installed new packages for serving static files in production, whiteNoise and Brotli, which have been added to requirements.txt file

--- workoutApp/serializers.py
+++ workoutApp/serializers.py
@@ -59,16 +59,4 @@ class ExercisesSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-#STEPS TO DEPLOYMENT ON RENDER
-# INSTALL pip install uvicorn gunicorn on your project terminal
-# install pip install dj-database-url , This package allows us to transform the database URL into Django database parameters.
-# Add this to requirements.txt file, like this; dj-database-url==2.3.0
-# Your 'DATABASE_URL': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600), will be in this format under DATABASES SETTINGS
-# To collect static files and migrate the database, we'll create a build script
-# Type your  pip install -r requirements.txt command again...
-# Add STATIC_ROOT = os.path.join(BASE_DIR, 'static') to your settings.py under STATIC_URL
-# Run python manage.py collectstatic --no-input
-# Run python manage.py migrate to migrate all changes...
-#
-
 
